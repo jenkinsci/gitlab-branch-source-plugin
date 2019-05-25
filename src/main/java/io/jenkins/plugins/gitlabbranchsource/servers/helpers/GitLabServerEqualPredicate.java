41a5252196285ea89bbd6539809dcf9db1ad46eb
Parichay Barpanda <parichay.barpanda@gmail.com>
2019-05-26 02:22:20 +0530

    added line feeds to all files

--- src/main/java/io/jenkins/plugins/gitlabbranchsource/servers/helpers/GitLabServerEqualPredicate.java
+++ src/main/java/io/jenkins/plugins/gitlabbranchsource/servers/helpers/GitLabServerEqualPredicate.java
@@ -4,7 +4,7 @@ package io.jenkins.plugins.gitlabbranchsource.servers.helpers;
 import io.jenkins.plugins.gitlabbranchsource.servers.GitLabServer;
 
 public class GitLabServerEqualPredicate implements GitLabServerPredicate {
-        public boolean test(String serverUrl, GitLabServer gitLabServer) {
-            return serverUrl.equals(gitLabServer.getServerUrl());
-        }
+    public boolean test(String serverUrl, GitLabServer gitLabServer) {
+        return serverUrl.equals(gitLabServer.getServerUrl());
     }
+}
