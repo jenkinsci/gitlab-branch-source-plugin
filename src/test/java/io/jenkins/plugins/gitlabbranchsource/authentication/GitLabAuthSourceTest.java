cedfe9c7a69745451459bd7d543a751d6d9a7edf
Parichay Barpanda <parichay.barpanda@gmail.com>
2019-05-28 15:34:38 +0530

    added tests

---
+++ src/test/java/io/jenkins/plugins/gitlabbranchsource/authentication/GitLabAuthSourceTest.java
@@ -0,0 +1,47 @@
+package io.jenkins.plugins.gitlabbranchsource.authentication;
+import com.cloudbees.plugins.credentials.common.UsernamePasswordCredentials;
+import hudson.util.Secret;
+import io.jenkins.plugins.gitlabbranchsource.api.client.GitLabAuth;
+import io.jenkins.plugins.gitlabbranchsource.api.client.GitLabAuthToken;
+import io.jenkins.plugins.gitlabbranchsource.api.client.GitLabAuthUser;
+import jenkins.authentication.tokens.api.AuthenticationTokens;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.mockito.Mockito;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+
+import io.jenkins.plugins.gitlabbranchsource.credentials.PersonalAccessToken;
+
+import static org.junit.Assert.assertThat;
+
+public class GitLabAuthSourceTest {
+    @ClassRule
+    public static JenkinsRule j = new JenkinsRule();
+
+    @Test
+    public void given__tokenCredential__when__convert__then__tokenAuth() throws Exception {
+        // we use a mock to ensure that java.lang.reflect.Proxy implementations of the credential interface work
+        PersonalAccessToken personalAccessToken = Mockito.mock(PersonalAccessToken.class);
+        Mockito.when(personalAccessToken.getToken()).thenReturn(Secret.fromString("sAfbXasnou47yxoAsCax"));
+        GitLabAuth gitLabAuth = AuthenticationTokens.convert(GitLabAuth.class, personalAccessToken);
+
+        assertThat(gitLabAuth, instanceOf(GitLabAuthToken.class));
+        assertThat(((GitLabAuthToken)gitLabAuth).getToken(), is("sAfbXasnou47yxoAsCax"));
+    }
+
+    @Test
+    public void given__userPassCredential__when__convert__then__tokenAuth() throws Exception {
+        // we use a mock to ensure that java.lang.reflect.Proxy implementations of the credential interface work
+        UsernamePasswordCredentials usernamePasswordCredential = Mockito.mock(UsernamePasswordCredentials.class);
+        Mockito.when(usernamePasswordCredential.getUsername()).thenReturn("alice");
+        Mockito.when(usernamePasswordCredential.getPassword()).thenReturn(Secret.fromString("ilovejenkins"));
+        GitLabAuth gitLabAuth = AuthenticationTokens.convert(GitLabAuth.class, usernamePasswordCredential);
+        assertThat(gitLabAuth, instanceOf(GitLabAuthUser.class));
+        assertThat(((GitLabAuthUser)gitLabAuth).getUsername(), is("alice"));
+        assertThat(((GitLabAuthUser)gitLabAuth).getPassword(), is("ilovejenkins"));
+    }
+
+}
