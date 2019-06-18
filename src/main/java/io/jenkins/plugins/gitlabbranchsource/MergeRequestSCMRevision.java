331d2601a85816e51644f7faf1a197f85086893d
Parichay Barpanda <parichay.barpanda@gmail.com>
2019-06-18 07:03:44 +0530

    Impl of MergeRequestSCMRevision

--- src/main/java/io/jenkins/plugins/gitlabbranchsource/MergeRequestSCMRevision.java
+++ src/main/java/io/jenkins/plugins/gitlabbranchsource/MergeRequestSCMRevision.java
@@ -1,4 +1,48 @@
 package io.jenkins.plugins.gitlabbranchsource;
 
-public class MergeRequestSCMRevision {
+import edu.umd.cs.findbugs.annotations.NonNull;
+import jenkins.scm.api.SCMRevision;
+import jenkins.scm.api.mixin.ChangeRequestSCMRevision;
+import org.kohsuke.stapler.export.Exported;
+
+public class MergeRequestSCMRevision extends ChangeRequestSCMRevision<MergeRequestSCMHead> {
+
+    private BranchSCMRevision origin;
+
+    /**
+     * Constructor.
+     *
+     * @param head   the {@link MergeRequestSCMHead} that the {@link SCMRevision} belongs to.
+     * @param target the {@link BranchSCMRevision} of the {@link MergeRequestSCMHead#getTarget()}.
+     * @param origin the {@link BranchSCMRevision} of the {@link MergeRequestSCMHead#getOrigin()} head.
+     */
+    protected MergeRequestSCMRevision(
+            @NonNull MergeRequestSCMHead head,
+            @NonNull BranchSCMRevision target,
+            @NonNull BranchSCMRevision origin) {
+        super(head, target);
+        this.origin = origin;
+    }
+
+    @Exported
+    @NonNull
+    public final BranchSCMRevision getOrigin() {
+        return origin;
+    }
+
+    @Override
+    public boolean equivalent(ChangeRequestSCMRevision<?> revision) {
+        return (revision instanceof MergeRequestSCMRevision)
+                && origin.equals(((MergeRequestSCMRevision) revision).getOrigin());
+    }
+
+    @Override
+    protected int _hashCode() {
+        return origin.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return (isMerge() ? ((BranchSCMRevision) getTarget()).getHash() + "+" : "") + origin.getHash();
+    }
 }
