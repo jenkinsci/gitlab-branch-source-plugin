93cc3284d70a636cbce9de6ba442881388675a88
Parichay Barpanda <parichay.barpanda@gmail.com>
2019-06-18 20:28:04 +0530

    added enum WebhookRegistration

--- src/main/java/io/jenkins/plugins/gitlabbranchsource/WebhookRegistration.java
+++ src/main/java/io/jenkins/plugins/gitlabbranchsource/WebhookRegistration.java
@@ -1,4 +1,22 @@
 package io.jenkins.plugins.gitlabbranchsource;
 
+import io.jenkins.plugins.gitlabserverconfig.servers.GitLabServers;
+
+/**
+ * Enumeration of the different webhook registration modes.
+ */
 public enum WebhookRegistration {
+    /**
+     * Disable webhook registration.
+     */
+    DISABLE,
+    /**
+     * Use the global system configuration for webhook registration. (If the {@link GitLabServers}
+     * does not have webhook registration configured then this will be the same as {@link #DISABLE})
+     */
+    SYSTEM,
+    /**
+     * Use the item scoped credentials to register the webhook.
+     */
+    ITEM
 }
