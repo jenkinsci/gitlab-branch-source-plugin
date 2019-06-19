1d4b1eecf78a600e7fd5e239f734db965f94dfae
Parichay Barpanda <parichay.barpanda@gmail.com>
2019-06-19 10:15:29 +0530

    Impl GitLabLink

--- src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabLink.java
+++ src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabLink.java
@@ -1,6 +1,99 @@
 package io.jenkins.plugins.gitlabbranchsource;
 
-public class GitLabLink implements hudson.model.Action {
-    public GitLabLink(String s, String expand) {
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.model.Action;
+import jenkins.model.Jenkins;
+import org.apache.commons.jelly.JellyContext;
+import org.jenkins.ui.icon.Icon;
+import org.jenkins.ui.icon.IconSet;
+import org.jenkins.ui.icon.IconSpec;
+import org.kohsuke.stapler.Stapler;
+
+/**
+ * Link to GitLab
+ */
+public class GitLabLink implements Action, IconSpec {
+    /**
+     * The icon class name to use.
+     */
+    @NonNull
+    private final String iconClassName;
+
+    /**
+     * Target of the hyperlink to take the user to.
+     */
+    @NonNull
+    private final String url;
+
+    public GitLabLink(@NonNull String iconClassName, @NonNull String url) {
+        this.iconClassName = iconClassName;
+        this.url = url;
     }
+
+    @NonNull
+    public String getUrl() {
+        return url;
+    }
+
+    @Override
+    public String getIconClassName() {
+        return iconClassName;
+    }
+
+    @Override
+    public String getIconFileName() {
+        String iconClassName = getIconClassName();
+        if (iconClassName != null) {
+            Icon icon = IconSet.icons.getIconByClassSpec(iconClassName + " icon-md");
+            if (icon != null) {
+                JellyContext ctx = new JellyContext();
+                ctx.setVariable("resURL", Stapler.getCurrentRequest().getContextPath() + Jenkins.RESOURCE_PATH);
+                return icon.getQualifiedUrl(ctx);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "GitLab";
+    }
+
+    @Override
+    public String getUrlName() {
+        return url;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = iconClassName.hashCode();
+        result = 31 * result + url.hashCode();
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        GitLabLink that = (GitLabLink) o;
+
+        if (!iconClassName.equals(that.iconClassName)) {
+            return false;
+        }
+        return url.equals(that.url);
+    }
+
+    @Override
+    public String toString() {
+        return "GitLabLink{" +
+                "iconClassName='" + iconClassName + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
+
 }
