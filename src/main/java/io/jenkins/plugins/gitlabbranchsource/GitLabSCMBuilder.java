e5b3574c04c6ab5c437de91ff9bc052fdeccd0aa
Parichay Barpanda <parichay.barpanda@gmail.com>
2019-06-21 02:25:59 +0530

    Import changes in GitLabSCMBuilder

--- src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabSCMBuilder.java
+++ src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabSCMBuilder.java
@@ -14,6 +14,7 @@ import hudson.model.Queue;
 import hudson.model.queue.Tasks;
 import hudson.plugins.git.GitSCM;
 import hudson.security.ACL;
+import io.jenkins.plugins.gitlabbranchsource.helpers.GitLabBrowser;
 import io.jenkins.plugins.gitlabserverconfig.credentials.PersonalAccessToken;
 import io.jenkins.plugins.gitlabserverconfig.servers.GitLabServer;
 import jenkins.plugins.git.GitSCMBuilder;
