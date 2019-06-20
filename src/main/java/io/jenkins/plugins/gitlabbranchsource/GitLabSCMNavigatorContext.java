8f38d8a9a57ea389db4b21a622830425e0317e58
Parichay Barpanda <parichay.barpanda@gmail.com>
2019-06-20 09:43:34 +0530

    Impl GitLabSCMNavigatorContext

--- src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabSCMNavigatorContext.java
+++ src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabSCMNavigatorContext.java
@@ -1,4 +1,14 @@
 package io.jenkins.plugins.gitlabbranchsource;
 
-public class GitLabSCMNavigatorContext {
+import edu.umd.cs.findbugs.annotations.NonNull;
+import jenkins.scm.api.SCMNavigator;
+import jenkins.scm.api.SCMSourceObserver;
+import jenkins.scm.api.trait.SCMNavigatorContext;
+
+public class GitLabSCMNavigatorContext extends SCMNavigatorContext<GitLabSCMNavigatorContext, GitLabSCMNavigatorRequest> {
+    @NonNull
+    @Override
+    public GitLabSCMNavigatorRequest newRequest(@NonNull SCMNavigator navigator, @NonNull SCMSourceObserver observer) {
+        return new GitLabSCMNavigatorRequest(navigator, this, observer);
+    }
 }
