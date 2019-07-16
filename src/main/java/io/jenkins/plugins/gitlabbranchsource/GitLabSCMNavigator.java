8933e6126686d20ff1b3501a6c203a3dfe289055
Parichay Barpanda <parichay.barpanda@gmail.com>
2019-07-16 12:51:17 +0530

    test

--- src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabSCMNavigator.java
+++ src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabSCMNavigator.java
@@ -54,7 +54,6 @@ import org.gitlab4j.api.models.Project;
 import org.gitlab4j.api.models.ProjectFilter;
 import org.gitlab4j.api.models.User;
 import org.gitlab4j.api.models.Visibility;
-import org.jenkins.ui.icon.IconSpec;
 import org.jenkinsci.plugins.gitclient.GitClient;
 import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
--- src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabSCMSource.java
+++ src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabSCMSource.java
@@ -75,7 +75,6 @@ import org.gitlab4j.api.models.Project;
 import org.gitlab4j.api.models.ProjectFilter;
 import org.gitlab4j.api.models.Tag;
 import org.gitlab4j.api.models.Visibility;
-import org.jenkins.ui.icon.IconSpec;
 import org.jenkinsci.plugins.gitclient.GitClient;
 import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
