c56e59ab1df19144a5f33ac33113954b8191beba
Parichay Barpanda <parichay.barpanda@gmail.com>
2019-08-12 23:41:18 +0530

    replaced databoundconstructor with databoundsetter in log comment trait

--- src/main/java/io/jenkins/plugins/gitlabbranchsource/LogCommentTrait.java
+++ src/main/java/io/jenkins/plugins/gitlabbranchsource/LogCommentTrait.java
@@ -9,6 +9,7 @@ import jenkins.scm.api.trait.SCMSourceTrait;
 import jenkins.scm.api.trait.SCMSourceTraitDescriptor;
 import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
 
 
 public class LogCommentTrait extends SCMSourceTrait {
@@ -20,7 +21,14 @@ public class LogCommentTrait extends SCMSourceTrait {
      * Constructor for stapler.
      */
     @DataBoundConstructor
-    public LogCommentTrait(@Nonnull String sudoUser) {
+    public LogCommentTrait() {
+    }
+
+    /**
+     * Setter for stapler to set the username of the sudo user.
+     */
+    @DataBoundSetter
+    public void setSudoUser(@NonNull String sudoUser) {
         this.sudoUser = sudoUser;
     }
 
@@ -33,6 +41,11 @@ public class LogCommentTrait extends SCMSourceTrait {
         }
     }
 
+    /**
+     * Getter method for username of sudo user.
+     *
+     * @return username of sudo user.
+     */
     @NonNull
     public String getSudoUser() {
         return sudoUser;
