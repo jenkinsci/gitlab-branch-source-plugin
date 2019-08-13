df45f787ddc60d0e5cd95a230eea5d1b1f2678e7
Parichay Barpanda <parichay.barpanda@gmail.com>
2019-08-13 23:12:09 +0530

    remove unused import

--- src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabSCMSourceRequest.java
+++ src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabSCMSourceRequest.java
@@ -10,7 +10,6 @@ import java.util.Collections;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import jenkins.scm.api.SCMHead;
