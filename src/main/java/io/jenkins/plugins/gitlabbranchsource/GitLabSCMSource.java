03081c1e106c0a029ddf35a66c9f8846216c5bd9
Joseph Petersen <josephp90@gmail.com>
2019-08-29 16:47:56 +0200

    add gitlab project avatar

--- src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabSCMSource.java
+++ src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabSCMSource.java
@@ -18,6 +18,7 @@ import hudson.model.queue.Tasks;
 import hudson.scm.SCM;
 import hudson.security.ACL;
 import hudson.util.ListBoxModel;
+import io.jenkins.plugins.gitlabbranchsource.helpers.GitLabAvatar;
 import io.jenkins.plugins.gitlabbranchsource.helpers.GitLabLink;
 import io.jenkins.plugins.gitlabserverconfig.credentials.PersonalAccessToken;
 import io.jenkins.plugins.gitlabserverconfig.servers.GitLabServer;
@@ -69,6 +70,7 @@ import jenkins.scm.impl.UncategorizedSCMHeadCategory;
 import jenkins.scm.impl.form.NamedArrayList;
 import jenkins.scm.impl.trait.Discovery;
 import jenkins.scm.impl.trait.Selection;
+import org.apache.commons.lang.StringUtils;
 import org.gitlab4j.api.Constants;
 import org.gitlab4j.api.GitLabApi;
 import org.gitlab4j.api.GitLabApiException;
@@ -547,6 +549,10 @@ public class GitLabSCMSource extends AbstractGitSCMSource {
         }
         result.add(new ObjectMetadataAction(gitlabProject.getNameWithNamespace(), gitlabProject.getDescription(),
             gitlabProject.getWebUrl()));
+        String avatarUrl = gitlabProject.getAvatarUrl();
+        if (StringUtils.isNotBlank(avatarUrl)) {
+            result.add(new GitLabAvatar(avatarUrl));
+        }
         String projectUrl = projectUriTemplate(serverName)
             .set("project", splitPath(projectPath))
             .expand();
