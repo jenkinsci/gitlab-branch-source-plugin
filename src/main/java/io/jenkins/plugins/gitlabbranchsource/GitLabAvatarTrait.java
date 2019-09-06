2495c938c5e964a61a0702d80616f8adbb2f5f2d
Parichay Barpanda <parichay.barpanda@gmail.com>
2019-09-06 18:14:34 +0530

    Impl GitLabAvatarTrait

---
+++ src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabAvatarTrait.java
@@ -0,0 +1,68 @@
+package io.jenkins.plugins.gitlabbranchsource;
+
+import hudson.Extension;
+import jenkins.scm.api.SCMSource;
+import jenkins.scm.api.trait.SCMSourceContext;
+import jenkins.scm.api.trait.SCMSourceTrait;
+import jenkins.scm.api.trait.SCMSourceTraitDescriptor;
+import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+
+public class GitLabAvatarTrait extends SCMSourceTrait {
+
+    /**
+     * Stores true or false to disable avatar for projects (but not owner)
+     */
+    private boolean disableProjectAvatar = false;
+
+    /**
+     * Constructor for stapler.
+     */
+    @DataBoundConstructor
+    public GitLabAvatarTrait() {
+        //empty
+    }
+
+    @DataBoundSetter
+    public void setDisableProjectAvatar(boolean disableProjectAvatar) {
+        this.disableProjectAvatar = disableProjectAvatar;
+    }
+
+    @Override
+    protected void decorateContext(SCMSourceContext<?, ?> context) {
+        if (context instanceof GitLabSCMSourceContext) {
+            GitLabSCMSourceContext ctx = (GitLabSCMSourceContext) context;
+            ctx.withProjectAvatarDisabled(doDisableProjectAvatar());
+        }
+    }
+
+    public boolean doDisableProjectAvatar() {
+        return disableProjectAvatar;
+    }
+
+    /**
+     * Our descriptor.
+     */
+    @Extension
+    @Symbol("gitlabAvatar")
+    public static class DescriptorImpl extends SCMSourceTraitDescriptor {
+
+        @Override
+        public String getDisplayName() {
+            return Messages.GitLabAvatarTrait_displayName();
+        }
+
+        @Override
+        public Class<? extends SCMSourceContext> getContextClass() {
+            return GitLabSCMSourceContext.class;
+        }
+
+        @Override
+        public Class<? extends SCMSource> getSourceClass() {
+            return GitLabSCMSource.class;
+        }
+
+    }
+
+}
