fbb14aa4e5284f00c5781eeb06dfa7fc60ae1c69
Parichay Barpanda <parichay.barpanda@gmail.com>
2019-09-07 14:59:46 +0530

    remove comment out statements

--- src/main/java/io/jenkins/plugins/gitlabbranchsource/helpers/GitLabAvatar.java
+++ src/main/java/io/jenkins/plugins/gitlabbranchsource/helpers/GitLabAvatar.java
@@ -12,29 +12,7 @@ public class GitLabAvatar extends AvatarMetadataAction {
     public GitLabAvatar(String avatar) {
         this.avatar = avatar;
     }
-
-//    @Override
-//    public String getAvatarImageOf(@NonNull String size) {
-//        if (avatar == null) {
-//            // fall back to the generic gitlab logo
-//            String image = avatarIconClassNameImageOf(getAvatarIconClassName(), size);
-//            return image != null
-//                ? image
-//                : (Stapler.getCurrentRequest().getContextPath() + Hudson.RESOURCE_PATH
-//                    + "/plugin/gitlab-branch-source/images/" + size + "/gitlab-logo.png");
-//        } else {
-//            String[] xy = size.split("x");
-//            if (xy.length == 0) {
-//                return avatar;
-//            }
-//            if (avatar.contains("?")) {
-//                return avatar + "&s=" + xy[0];
-//            } else {
-//                return avatar + "?s=" + xy[0];
-//            }
-//        }
-//    }
-
+    
     @Override
     public String getAvatarImageOf(@NonNull String size) {
         return StringUtils.isBlank(avatar) ? null : GitLabAvatarCache.buildUrl(avatar, size);
