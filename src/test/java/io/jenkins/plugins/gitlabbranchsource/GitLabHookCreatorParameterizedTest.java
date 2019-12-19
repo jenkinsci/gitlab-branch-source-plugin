29270df96d8c97af253923836eccf468a590aa8b
Joseph Petersen <josephp90@gmail.com>
2019-12-19 14:56:12 +0100

    add unit test for hook url

---
+++ src/test/java/io/jenkins/plugins/gitlabbranchsource/GitLabHookCreatorParameterizedTest.java
@@ -0,0 +1,55 @@
+package io.jenkins.plugins.gitlabbranchsource;
+
+import java.util.Arrays;
+import jenkins.model.JenkinsLocationConfiguration;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(Parameterized.class)
+public class GitLabHookCreatorParameterizedTest {
+
+    @ClassRule
+    public static JenkinsRule r = new JenkinsRule();
+
+    private final String jenkinsUrl;
+    private final boolean hookType;
+    private final String expectedPath;
+
+    @Parameters(name = "check {0}")
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+            {"intranet.local:8080", false, "/gitlab-systemhook/post"},
+            {"intranet.local", true, "/gitlab-webhook/post"},
+            {"www.mydomain.com:8000", true, "/gitlab-webhook/post"},
+            {"www.mydomain.com", false, "/gitlab-systemhook/post"}
+        });
+    }
+
+    public GitLabHookCreatorParameterizedTest(String jenkinsUrl, boolean hookType, String expectedPath) {
+        this.jenkinsUrl = jenkinsUrl;
+        this.hookType = hookType;
+        this.expectedPath = expectedPath;
+    }
+
+    @Test
+    public void hookUrl() {
+        Arrays.asList("http://", "https://").forEach(
+            proto -> {
+                String expected = proto + jenkinsUrl + expectedPath;
+                JenkinsLocationConfiguration.get().setUrl(proto + jenkinsUrl);
+                String hookUrl = GitLabHookCreator.getHookUrl(hookType);
+                GitLabHookCreator.checkURL(hookUrl);
+                assertThat(hookUrl.replaceAll(proto, ""), not(containsString("//")));
+                assertThat(hookUrl, is(expected));
+            });
+    }
+}
