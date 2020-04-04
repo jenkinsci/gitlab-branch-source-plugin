008124361153d7c58d287344720f694229a286ca
Joseph Petersen <josephp90@gmail.com>
2020-04-04 11:59:12 +0200

    ignore super class shadowing

--- src/main/java/io/jenkins/plugins/gitlabbranchsource/MergeWithGitSCMExtension.java
+++ src/main/java/io/jenkins/plugins/gitlabbranchsource/MergeWithGitSCMExtension.java
@@ -2,6 +2,7 @@ package io.jenkins.plugins.gitlabbranchsource;
 
 import edu.umd.cs.findbugs.annotations.CheckForNull;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.io.ObjectStreamException;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
@@ -13,6 +14,7 @@ import org.kohsuke.accmod.restrictions.NoExternalUse;
  */
 @Deprecated
 @Restricted(NoExternalUse.class)
+@SuppressFBWarnings(value = "NM_SAME_SIMPLE_NAME_AS_SUPERCLASS")
 public class MergeWithGitSCMExtension extends jenkins.plugins.git.MergeWithGitSCMExtension {
 
     MergeWithGitSCMExtension(@NonNull String baseName, @CheckForNull String baseHash) {
