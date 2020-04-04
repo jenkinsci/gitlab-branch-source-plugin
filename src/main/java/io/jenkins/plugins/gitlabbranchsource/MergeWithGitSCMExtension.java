04fde395b9eb94725d3f4fad258b3f17923e32fd
Parichay Barpanda <parichay.barpanda@gmail.com>
2020-04-04 14:36:35 +0530

    Add bom to dep mgmt, upgrade plugin versions

--- src/main/java/io/jenkins/plugins/gitlabbranchsource/MergeWithGitSCMExtension.java
+++ src/main/java/io/jenkins/plugins/gitlabbranchsource/MergeWithGitSCMExtension.java
@@ -2,23 +2,7 @@ package io.jenkins.plugins.gitlabbranchsource;
 
 import edu.umd.cs.findbugs.annotations.CheckForNull;
 import edu.umd.cs.findbugs.annotations.NonNull;
-import hudson.model.Run;
-import hudson.model.TaskListener;
-import hudson.plugins.git.GitException;
-import hudson.plugins.git.GitSCM;
-import hudson.plugins.git.Revision;
-import hudson.plugins.git.extensions.GitSCMExtension;
-import hudson.plugins.git.extensions.impl.PreBuildMerge;
-import hudson.plugins.git.util.MergeRecord;
-import java.io.IOException;
 import java.io.ObjectStreamException;
-import org.apache.commons.lang.StringUtils;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.ObjectId;
-import org.jenkinsci.plugins.gitclient.CheckoutCommand;
-import org.jenkinsci.plugins.gitclient.GitClient;
-import org.jenkinsci.plugins.gitclient.MergeCommand;
-import org.kohsuke.accmod.restrictions.DoNotUse;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
 
@@ -28,7 +12,7 @@ import org.kohsuke.accmod.restrictions.NoExternalUse;
  * @deprecated use {@link jenkins.plugins.git.MergeWithGitSCMExtension}
  */
 @Deprecated
-@Restricted(DoNotUse.class)
+@Restricted(NoExternalUse.class)
 public class MergeWithGitSCMExtension extends jenkins.plugins.git.MergeWithGitSCMExtension {
 
     MergeWithGitSCMExtension(@NonNull String baseName, @CheckForNull String baseHash) {
