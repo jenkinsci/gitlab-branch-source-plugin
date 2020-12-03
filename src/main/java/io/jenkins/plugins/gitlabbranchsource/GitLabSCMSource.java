46379dbc41a86d2f3d10a8858b340a2141d5b102
Artur Dick <artur.dick@adition.com>
2020-12-03 14:57:20 +0100

    Spacing

--- src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabSCMSource.java
+++ src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabSCMSource.java
@@ -732,6 +732,7 @@ public class GitLabSCMSource extends AbstractGitSCMSource {
                 Jenkins.getAuthentication(), fromUri(getServerUrlFromName(serverName)).build()),
                 GitLabServer.CREDENTIALS_MATCHER);
     }
+
     @Symbol("gitlab")
     @Extension
     public static class DescriptorImpl extends SCMSourceDescriptor implements IconSpec {
