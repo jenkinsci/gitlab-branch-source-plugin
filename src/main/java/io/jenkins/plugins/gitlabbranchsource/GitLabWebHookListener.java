39d04829121f86f51f8ffa25e47bf544f43883ac
Alexander Schmidt <alex@treefish.org>
2021-06-30 14:16:38 +0200

    Implemented trigger delay logic

--- src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabWebHookListener.java
+++ src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabWebHookListener.java
@@ -1,5 +1,6 @@
 package io.jenkins.plugins.gitlabbranchsource;
 
+import hudson.util.VersionNumber;
 import io.jenkins.plugins.gitlabserverconfig.servers.GitLabServer;
 import io.jenkins.plugins.gitlabserverconfig.servers.GitLabServers;
 import java.net.URI;
@@ -20,8 +21,11 @@ import static io.jenkins.plugins.gitlabbranchsource.helpers.GitLabHelper.apiBuil
 public class GitLabWebHookListener implements WebHookListener {
     public static final Logger LOGGER = Logger.getLogger(GitLabWebHookListener.class.getName());
 
-    // Delay used for triggers to avoid GitLab cache
-    private static final long TRIGGER_DELAY_SECONDS = 30;
+    // GitLab API uses caching for branches endpoint from this version on
+    private static final String MIN_CACHING_GITLAB_VERSION = "13.12.0";
+
+    // GitLab API caching timeout used for branches endpoint
+    private static final long GITLAB_CACHING_TIMEOUT = 30;
 
     private String origin;
 
@@ -32,55 +36,90 @@ public class GitLabWebHookListener implements WebHookListener {
     @Override
     public void onNoteEvent(NoteEvent noteEvent) {
         LOGGER.log(Level.FINE, noteEvent.toString());
-        GitLabMergeRequestCommentTrigger trigger = new GitLabMergeRequestCommentTrigger(noteEvent);
+        GitLabMergeRequestCommentTrigger trigger =
+            new GitLabMergeRequestCommentTrigger(noteEvent);
         AbstractGitLabJobTrigger.fireNow(trigger);
     }
 
     @Override
     public void onMergeRequestEvent(MergeRequestEvent mrEvent) {
         LOGGER.log(Level.FINE, mrEvent.toString());
-        //mrEvent.getProject().getWebUrl()
+        final long triggerDelay = findTriggerDelay(mrEvent.getProject().getWebUrl());
         GitLabMergeRequestSCMEvent trigger = new GitLabMergeRequestSCMEvent(mrEvent, origin);
-        SCMHeadEvent.fireLater(trigger, TRIGGER_DELAY_SECONDS, TimeUnit.SECONDS);
+        SCMHeadEvent.fireLater(trigger, triggerDelay, TimeUnit.SECONDS);
     }
 
     @Override
     public void onPushEvent(PushEvent pushEvent) {
         LOGGER.log(Level.FINE, pushEvent.toString());
+        final long triggerDelay = findTriggerDelay(pushEvent.getProject().getWebUrl());
         GitLabPushSCMEvent trigger = new GitLabPushSCMEvent(pushEvent, origin);
-        SCMHeadEvent.fireLater(trigger, TRIGGER_DELAY_SECONDS, TimeUnit.SECONDS);
+        SCMHeadEvent.fireLater(trigger, triggerDelay, TimeUnit.SECONDS);
     }
 
     @Override
     public void onTagPushEvent(TagPushEvent tagPushEvent) {
         LOGGER.log(Level.FINE, tagPushEvent.toString());
+        final long triggerDelay = findTriggerDelay(tagPushEvent.getProject().getWebUrl());
         GitLabTagPushSCMEvent trigger = new GitLabTagPushSCMEvent(tagPushEvent, origin);
-        SCMHeadEvent.fireLater(trigger, TRIGGER_DELAY_SECONDS, TimeUnit.SECONDS);
+        SCMHeadEvent.fireLater(trigger, triggerDelay, TimeUnit.SECONDS);
     }
 
     private long findTriggerDelay(final String projectUrl) {
-        GitlabServer projectServer = null;
+        final GitLabServer projectServer = findProjectServer(projectUrl);
+
+        if (projectServer == null) {
+            return GITLAB_CACHING_TIMEOUT;
+        }
+
+        final Integer configuredDelay = projectServer.getHookTriggerDelay();
+        if (configuredDelay != null) {
+            return configuredDelay;
+        }
+
+        final Version gitLabVersion = retrieveGitLabVersion(projectServer);
+        final Boolean isCachingGitLabVersion = isCachingGitLabVersion(gitLabVersion);
+
+        if (isCachingGitLabVersion == null || isCachingGitLabVersion) {
+            return GITLAB_CACHING_TIMEOUT;
+        } else {
+            return 0;
+        }
+    }
+
+    private GitLabServer findProjectServer(final String projectUrl) {
         for (GitLabServer server: GitLabServers.get().getServers()) {
             if (projectUrl.startsWith(server.getServerUrl())) {
-                projectServer = server;
-                break;
+                return server;
             }
         }
-        if (projectServer != null) {
-            final Integer delay = projectServer.getHookTriggerDelay();
-            if (delay != null) {
-                return delay;
-            } else {
-                try {
-                    final Version gitLabVersion = apiBuilder(serverName).getVersion();
-                    // check version...
-                } catch (GitLabApiException e) {
-                    LOGGER.log(Level.WARNING, String.format("Error retrieving GitLab version: %s", e.getMessage()));
-                    return TRIGGER_DELAY_SECONDS;
-                }
-            }
-        } else {
-            return TRIGGER_DELAY_SECONDS;
+        LOGGER.log(Level.WARNING,
+                   String.format("No GitLab server for project URL: %s", projectUrl));
+        return null;
+    }
+
+    private Version retrieveGitLabVersion(final GitLabServer server) {
+        try {
+            return apiBuilder(server.getName()).getVersion();
+        } catch (GitLabApiException e) {
+            LOGGER.log(
+                Level.WARNING,
+                String.format("Error retrieving GitLab version: %s", e.getMessage()));
+            return null;
+        }
+    }
+
+    private Boolean isCachingGitLabVersion(final Version gitLabVersion) {
+        if (gitLabVersion == null) {
+            return null;
+        }
+        try {
+            return new VersionNumber(gitLabVersion.getVersion())
+                .isNewerThanOrEqualTo(new VersionNumber(MIN_CACHING_GITLAB_VERSION));
+        } catch (Exception e) {
+            LOGGER.log(Level.WARNING,
+                       String.format("Error comparing GitLab versions: %s", e.getMessage()));
+            return null;
         }
     }
 }
