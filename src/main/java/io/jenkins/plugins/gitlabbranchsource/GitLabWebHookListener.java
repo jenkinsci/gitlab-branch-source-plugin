6731859013cbb399d48fab129dc1bc1e9351871e
Alexander Schmidt <alex@treefish.org>
2021-07-05 23:40:55 +0200

    Removing GitLab API version check for trigger delay

--- src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabWebHookListener.java
+++ src/main/java/io/jenkins/plugins/gitlabbranchsource/GitLabWebHookListener.java
@@ -1,6 +1,5 @@
 package io.jenkins.plugins.gitlabbranchsource;
 
-import hudson.util.VersionNumber;
 import io.jenkins.plugins.gitlabserverconfig.servers.GitLabServer;
 import io.jenkins.plugins.gitlabserverconfig.servers.GitLabServers;
 import java.util.concurrent.TimeUnit;
@@ -13,19 +12,11 @@ import org.gitlab4j.api.webhook.PushEvent;
 import org.gitlab4j.api.webhook.TagPushEvent;
 import org.gitlab4j.api.webhook.WebHookListener;
 
-import static io.jenkins.plugins.gitlabbranchsource.helpers.GitLabHelper.apiBuilder;
-
 public class GitLabWebHookListener implements WebHookListener {
     public static final Logger LOGGER = Logger.getLogger(GitLabWebHookListener.class.getName());
 
-    // GitLab API uses caching for branches endpoint from this version on
-    private static final String MIN_CACHING_GITLAB_VERSION = "13.12.0";
-
     // GitLab API caching timeout used for branches endpoint
     private static final long GITLAB_CACHING_TIMEOUT = 30;
-
-    private static final String TRIGGER_DELAY_FALLBACK_MESSAGE =
-        "Falling back to default trigger delay equal GitLab caching timeout";
 
     private String origin;
 
@@ -69,26 +60,17 @@ public class GitLabWebHookListener implements WebHookListener {
         final GitLabServer projectServer = findProjectServer(projectUrl);
 
         if (projectServer == null) {
-            LOGGER.log(Level.WARNING, TRIGGER_DELAY_FALLBACK_MESSAGE);
+            LOGGER.log(
+                Level.WARNING,
+                "Falling back to default trigger delay equal GitLab caching timeout");
             return GITLAB_CACHING_TIMEOUT;
         }
 
         final Integer configuredDelay = projectServer.getHookTriggerDelay();
         if (configuredDelay != null) {
             return configuredDelay;
-        }
-
-        final VersionNumber gitLabVersion = retrieveGitLabVersion(projectServer);
-
-        if (gitLabVersion == null) {
-            LOGGER.log(Level.WARNING, TRIGGER_DELAY_FALLBACK_MESSAGE);
-            return GITLAB_CACHING_TIMEOUT;
-        }
-
-        if (isCachingGitLabVersion(gitLabVersion)) {
-            return GITLAB_CACHING_TIMEOUT;
         } else {
-            return 0;
+            return GITLAB_CACHING_TIMEOUT;
         }
     }
 
@@ -101,36 +83,5 @@ public class GitLabWebHookListener implements WebHookListener {
         LOGGER.log(Level.WARNING,
                    String.format("No GitLab server for project URL: %s", projectUrl));
         return null;
-    }
-
-    private VersionNumber retrieveGitLabVersion(final GitLabServer server) {
-        String versionString = null;
-        try {
-            /* Using API is not allowed here and fails with:
-             *   hudson.security.AccessDeniedException3:
-             *   anonymous is missing the Job/Build permission
-             * So API versions should be retrieved and cached earlier from
-             * somewhere else (where?) and cached versions should be used here.
-             */
-            versionString = apiBuilder(server.getName()).getVersion().getVersion();
-        } catch (Exception e) {
-            LOGGER.log(
-                Level.WARNING,
-                String.format("Error retrieving GitLab version: %s", e.getMessage()));
-            return null;
-        }
-        try {
-            return new VersionNumber(versionString);
-        } catch (Exception e) {
-            LOGGER.log(
-                Level.WARNING,
-                String.format("Error parsing GitLab version: %s", e.getMessage()));
-            return null;
-        }
-    }
-
-    private boolean isCachingGitLabVersion(final VersionNumber gitLabVersion) {
-        return gitLabVersion
-            .isNewerThanOrEqualTo(new VersionNumber(MIN_CACHING_GITLAB_VERSION));
     }
 }
