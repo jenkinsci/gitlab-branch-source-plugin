592d94d67b2a10e79d4aa89ddbc987989ba65b00
Pierre Baumard <pierre.baumard@cnav.fr>
2024-10-29 17:30:57 +0100

    Fix JENKINS-71955
    NullPointerException: Cannot invoke "java.lang.Boolean.booleanValue()" because the return value of "org.gitlab4j.api.models.Project.getMergeRequestsEnabled()" is null

---
+++ src/test/java/io/jenkins/plugins/gitlabbranchsource/GitLabSCMSourceTest.java
@@ -0,0 +1,72 @@
+package io.jenkins.plugins.gitlabbranchsource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Set;
+
+import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.MergeRequestApi;
+import org.gitlab4j.api.ProjectApi;
+import org.gitlab4j.api.RepositoryApi;
+import org.gitlab4j.api.models.Project;
+import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.RestartableJenkinsRule;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import hudson.model.TaskListener;
+import hudson.security.AccessControlled;
+import hudson.util.StreamTaskListener;
+import io.jenkins.plugins.gitlabbranchsource.helpers.GitLabHelper;
+import io.jenkins.plugins.gitlabserverconfig.servers.GitLabServer;
+import io.jenkins.plugins.gitlabserverconfig.servers.GitLabServers;
+import jenkins.branch.BranchSource;
+import jenkins.scm.api.SCMHead;
+
+public class GitLabSCMSourceTest {
+
+    private static final String SERVER = "server";
+    private static final String PROJECT_NAME = "project";
+    private static final String SOURCE_ID = "id";
+
+    @Rule
+    public final RestartableJenkinsRule plan = new RestartableJenkinsRule();
+
+    @Test
+    public void retrieveMRWithEmptyProjectSettings() {
+        plan.then(j -> {
+            GitLabApi gitLabApi = Mockito.mock(GitLabApi.class);
+            ProjectApi projectApi = Mockito.mock(ProjectApi.class);
+            RepositoryApi repoApi = Mockito.mock(RepositoryApi.class);
+            MergeRequestApi mrApi = Mockito.mock(MergeRequestApi.class);
+            Mockito.when(gitLabApi.getProjectApi()).thenReturn(projectApi);
+            Mockito.when(gitLabApi.getMergeRequestApi()).thenReturn(mrApi);
+            Mockito.when(gitLabApi.getRepositoryApi()).thenReturn(repoApi);
+            Mockito.when(projectApi.getProject(any())).thenReturn(new Project());
+            try (MockedStatic<GitLabHelper> utilities = Mockito.mockStatic(GitLabHelper.class)) {
+                utilities.when(() -> GitLabHelper.apiBuilder(any(AccessControlled.class), anyString()))
+                        .thenReturn(gitLabApi);
+                GitLabServers.get().addServer(new GitLabServer("", SERVER, ""));
+                GitLabSCMSourceBuilder sb = new GitLabSCMSourceBuilder(SOURCE_ID, SERVER, "creds", "po",
+                        "group/project",
+                        "project");
+                WorkflowMultiBranchProject project = j.createProject(WorkflowMultiBranchProject.class, PROJECT_NAME);
+                BranchSource source = new BranchSource(sb.build());
+                source.getSource()
+                        .setTraits(Arrays.asList(new BranchDiscoveryTrait(0), new OriginMergeRequestDiscoveryTrait(1)));
+                project.getSourcesList().add(source);
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                final TaskListener listener = new StreamTaskListener(out, StandardCharsets.UTF_8);
+                Set<SCMHead> scmHead = source.getSource().fetch(listener);
+                assertEquals(0, scmHead.size());
+            }
+        });
+    }
+}
